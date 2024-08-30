ae29f5cc2e545057d1c8210d5f75833254e6eaa4
jfechazu <jfechazu@gmail.com>
2024-08-30 11:20:52 -0300

    Add files via upload

---
+++ minesweeper.py
@@ -0,0 +1,43 @@
+import sys
+
+def get_python_version():
+    python_version = sys.version
+    print(python_version)
+
+def minesweeper(board):
+    rows = len(board)
+    cols = len(board[0])
+    final_board = [[0]*cols for _ in range(rows)]
+    
+    # Directions for all 8 neighbors (up, down, left, right, and diagonals)
+    directions = [(-1, -1), (-1, 0), (-1, 1),
+                  (0, -1),         (0, 1),
+                  (1, -1), (1, 0), (1, 1)]
+    
+    for r in range(rows):
+        for c in range(cols):
+            if board[r][c] == 1:
+                final_board[r][c] = 9
+            else:
+                # count numbers of mines around the cell
+                mine_count = 0
+                for dr, dc in directions:
+                    # Moving inside de board
+                    nr, nc = r + dr, c + dc
+                    # If it is inside the board and the is a mine count it
+                    if 0 <= nr < rows and 0 <= nc < cols and board[nr][nc] == 1:
+                        mine_count += 1
+                final_board[r][c] = mine_count
+    return final_board
+    
+    
+if __name__ == "__main__":
+    #get_python_version()
+    board = [
+    [0, 0, 0, 0],
+    [1, 0, 1, 0],
+    [1, 1, 0, 1]
+    ]
+    result = minesweeper(board)
+    for x in range (0,len(result)):
+        print(result[x])
